82a3402bff2bdf38ce4d33b9daefe1288ed46735
rampalliprakhar <rampalliprakhar@gmail.com>
2024-12-12 20:57:28 -0500

    Updated connection management

--- app.js
+++ app.js
@@ -10,18 +10,27 @@ const io = socket(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+let connectedUsers = {};    // store connected users
+
 // Handle socket connections
 io.on("connection", (socket) => {
     console.log("New user connected: " + socket.id);
-    socket.broadcast.emit("user-online", { id: socket.id }); // Notify others when a user connects
+    
+    // Add users to connectedUsers
+    connectedUsers[socket.id] = {id: socket.id, location:null};
 
+    // Notify users when a new user connects
+    socket.broadcast.emit("user-online", { id: socket.id });
+    
     // Listen for location updates from the client
     socket.on("send-location", (data) => {
+        connectedUsers[socket.id].location = data;
         socket.broadcast.emit("receive-location", { id: socket.id, ...data }); // Broadcast location to others
     });
 
     socket.on("disconnect", () => {
         console.log("User disconnected: " + socket.id);
+        delete connectedUsers[socket.id]; // Remove the user from the connectedUsers
         socket.broadcast.emit("user-offline", { id: socket.id }); // Notify others when a user disconnects
     });
 });
