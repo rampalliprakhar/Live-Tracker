321f8756d056fcf6701ec5e9eb75a433901e955e
rampalliprakhar <rampalliprakhar@gmail.com>
2024-12-25 20:42:29 -0500

    Updated Code

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
